253b9343fd25637b0def1e07d31c15fa7770b853
Hui Li <withlihui@gmail.com>
2016-09-03 16:32:59 +0800

    init

---
+++ js/typing.js
@@ -0,0 +1,43 @@
+// Typing is
+// Author:
+// Github
+
+var b = 0;
+var c = 0;
+var isTyping = true;
+var isLoop = true;
+var i = 0;
+var typingSpeed = 100;
+var deleteSpeed = 40;
+var waitTime = 600
+setTimeout("typing()", waitTime);
+
+function typing() {
+	document.getElementById("words").innerHTML = strings[i].substring(c, b);
+	if (b == strings[i].length){
+	    setTimeout("b=0, c=strings[i].length, isTyping=true", waitTime);
+	} else {
+		if (c != 0) {
+			c--;
+			if ((i == strings.length-1) && (isLoop==false)) {
+				return;
+			}
+
+			if (c == 0) {
+				i++;
+				if (i == strings.length) {
+					i=0;
+				}
+			}
+		} else {
+			var isTyping=false;
+			b++;
+			}
+	}
+
+	if (isTyping == false) {
+		setTimeout("typing()", typingSpeed);
+	} else {
+		setTimeout("typing()", deleteSpeed);
+	}
+}
